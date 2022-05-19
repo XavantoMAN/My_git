50ff1070d739bfbc45cfc4e977c38c17cbb3d2f7
XavantoMAN <sim.d.03@mail.ru>
2022-05-20 00:57:58 +0600

    2

--- my_git.py
+++ my_git.py
@@ -1,15 +1,16 @@
 import platform
 import os
 import shutil
+import re
 
 print('OS:', platform.system())
 Dir = os.path.dirname(os.path.abspath(__file__))
-commit = 0
 Flag = True
+lines = []
+newDir = ''
 
 
 def init():
-    global commit
     command = input(">")
     if command == "init":
         if os.path.exists("my_git"):
@@ -17,7 +18,7 @@ def init():
         else:
             os.mkdir("my_git")
         ignore = open('my_git/ignore.txt', 'w')
-        ignore.write('ignore.txt\ninit.txt\n*.cmd\n')
+        ignore.write('.git\n.vs\n.idea\nignore.txt\ninit.txt\n*.cmd\nmy_git\nCommites\nbin\n')
         ignore.close()
         with open('my_git/init.txt', 'w') as f1:
             dir_all(Dir, f1)
@@ -28,69 +29,109 @@ def init():
         temp = open('my_git/temp.txt', 'w')
         dir_all(Dir, temp)
         temp.close()
-        f1 = open('my_git/init.txt', 'r')
+        file1 = open('my_git/init.txt', 'r')
         f2 = open('my_git/temp.txt', 'r')
         f3 = open('my_git/ignore.txt', 'w')
-        f3.write('ignore.txt\ninit.txt\n*.cmd\nmy_git\n.idea\n')
-        for i in f1:
-            print(i)
+        f3.write('.git\n.vs\n.idea\nignore.txt\ninit.txt\n*.cmd\nmy_git\nCommites\nbin\n')
+        for i in file1:
             x = f2.readline()
+            #print('i=' + i)
+            #print('X=' + x + '-----')
             if x == i:
-                f3.write(x)
-        f1.close()
+                line2 = x.split(';')
+                f3.write(line2[0] + '\n')
+        file1.close()
         f2.close()
         f3.close()
-        f1 = open('my_git/init.txt', 'w')
-        f2 = open('my_git/temp.txt', 'r')
-        for i in f2:
-            x = f2.readline()
-            f1.write(x)
-        f1.close()
-        f2.close()
-        if os.path.exists('my_git/commit.txt'):
-            com = open('my_git/commit.txt', 'r')
-            commit = int(com.readline())
-            com.close()
-        else:
-            com = open('my_git/commit.txt', 'w')
-            commit = 0
-            com.write(str(commit))
-            com.close()
-        commit += 1
         ignore = open('my_git/ignore.txt', 'r')
-        list1 = ''
-        for i in ignore:
-            list1 += i
-        list2 = list1.split("\n")
-        line = f'Commites/commit{commit}'
-        shutil.copytree(f'.', line, ignore=shutil.ignore_patterns('ignore.txt', 'init.txt', '*cmd', 'my_git', '.idea',
-                                                                  'Commites', 'bin'))
-        f1 = open('my_git/commit.txt', 'w')
-        f1.write(str(commit))
-        f1.close()
+        global lines
+        while True:
+            s = ignore.readline()
+            line1 = re.sub('\n', '', s)
+            if not line1:
+                break
+            #print(line1)
+            lines.append(line1)
+
+        ignore.close()
+        with open('my_git/init.txt', 'w') as f1:
+            dir_all(Dir, f1)
+        name_commit = input("Введите название коммита:")
+        com_dir = os.path.join(Dir, 'Commites', name_commit)
+        if not os.path.exists(com_dir):
+            os.mkdir(com_dir)
+            commit_tree(Dir, com_dir)
+            init()
+        else:
+            print('Commit exists!')
 
     else:
         print(f"{command} не является командой или оператором")
         init()
 
 
-def dir_all(dir, f1):
-    dirfiles = os.listdir(dir)
+def ignore(f):
+    global lines
+    for item in lines:
+        if f == item:
+            return True
+            
+    return False
+
+
+def dir_all(Dir, f1):
+    dirfiles = os.listdir(Dir)
     for item in dirfiles:
-        path = os.path.join(dir, item)
+        path = os.path.join(Dir, item)
         if os.path.isfile(path):
-            print('File:', item)
-            print(path)
-            print(os.path.getmtime(path))
-            print(str(os.path.getsize(path)) + ' byte')
-            print(os.path.dirname(path))
-            print(os.path.basename(path))
-            f1.write(path + ';' + str(os.path.getmtime(path)))
+            f1.write(os.path.basename(path) + ';' + str(os.path.getmtime(path)))
             f1.write("\n")
-            print('---------------------')
+        elif os.path.isdir(path):
+            dir_all(path, f1)
+
+
+def fill_dirs(Dir, line):
+    dirfiles = os.listdir(Dir)
+    for item in dirfiles:
+        if ignore(item):
+            continue
+
+        path = os.path.join(Dir, item)
+        if os.path.isfile(path):
+            shutil.copy(path, line)
         elif os.path.isdir(path):
             print('Directory:', item)
-            dir_all(path, f1)
+            newDir = os.path.join(line, item)
+            os.mkdir(newDir)
+            commit_tree(path, line)
+
+
+def commit_tree(Dir, line):
+    global newDir
+    dirfiles = os.listdir(Dir)
+    for item in dirfiles:
+        #print(item)
+        if ignore(item):
+            continue
+
+        path = os.path.join(Dir, item)
+        if os.path.isfile(path):
+            shutil.copy(path, line)
+        elif os.path.isdir(path):
+            continue
+
+    for item in dirfiles:
+        if ignore(item):
+            continue
+
+        path = os.path.join(Dir, item)
+        if os.path.isfile(path):
+            continue
+        elif os.path.isdir(path):
+            print('Directory:', item)
+            newDir = os.path.join(line, item)
+            os.mkdir(newDir)
+            fill_dirs(path, newDir)
 
 
 while Flag:
